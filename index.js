d0585c68f275109f0d650c7a6474851ef3cd7a67
ajaymehul <aanbuselvam@crimson.ua.edu>
2019-05-20 14:03:22 -0500

    updated index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ if(process.argv.length === 2){
   console.log('Pass JSON file as argument to host.');
   console.log('use "json-server --help" for example');
 }
-else if(process.argv[2] === "--help"){
-  console.log('help will arrive');
-}
 else if(process.argv.length === 3){
   console.log('\x1b]37m\x1b[42m%s\x1b[0m','hosting JSON...\n');
   jsonServer(process.argv[2]);
